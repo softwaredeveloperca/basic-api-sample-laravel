eed5f796eb107449f941f20fdc7545850f80c3de
root <root@vultr.guest>
2019-05-09 15:29:43 +0000

    adding logic for deciding which notification to add

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\InfusionsoftHelper;
+use App\Module;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,7 @@ use App\Http\Helpers\InfusionsoftHelper;
 |
 */
 Route::post('module_reminder_assigner', function(Request $request) {
+	
     $InfusionHelper=new InfusionsoftHelper();
 
     if (!Cache::has('all_tags')){
@@ -21,14 +23,35 @@ Route::post('module_reminder_assigner', function(Request $request) {
 	});
     }
 
+    $all_tags=collect(Cache::get('all_tags'))->sortBy('name');
+    
     if($request->has('contact_email'))
     {
 	$contact=$InfusionHelper->getContact($request->contact_email);
 	if(empty($contact))
 	{
-		return response()->json(null, 404);
+		return response()->json(false, 404);
 	}    
-	return response()->json($contact, 204);	
+
+	$modules=explode(",", $contact['Groups']);
+	$courses=explode(",", $contact['_Products']);
+
+	collect(Cache::get('all_tags'))
+			->sortBy('name')
+			->each(function ($tag, $key) use ($courses, $modules, $contact) {
+			foreach($courses as $course_key => $course)
+			{
+			    if(str_contains($tag['name'], strtoupper($course))
+			       && !(in_array($tag['id'], $modules))
+			    ){
+				    dd($tag['name']);
+				 $InfusionHelper->addTag($contact['id'], $tag['id']);
+
+		                 return response()->json("Success", 204);
+			    } 				     
+			}	  
+        });
     }
+
     return response()->json(null, 400);
 });
